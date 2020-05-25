e8f2dbdbaa979f9d3fd5b7bbc774233c975fb2e5
Ben Sampica <benjamin.sampica@gmail.com>
2020-05-24 21:32:46 -0500

    Update DeckOfCards.cs
    Removed `init()` in favor of a constructor. Whenever a `new Deck()` is initialized, this will ensure the object is properly formed for the creator. Much better than `init()` because `init()` requires knowledge of the internals of the class `Deck` to know that `init()` must be
    called before `draw` or `shuffle`. 
    
    There's a concept of 'falling into a pit of success', where good design intent helps developers new and old (or even yourself coming back to code after a while) be successful almost by accident.

--- DeckOfCards.cs
+++ DeckOfCards.cs
@@ -6,19 +6,17 @@ namespace DeckOfCards
     class Deck
     {
         public List<Card> deckOfCards = new List<Card>();//List for 52 Cards, currently empty
-
-        //public currentCard;                            //Card to be dealt/drawn
-
-        public void init()
-        {
-            foreach (Card.Suit suit in Enum.GetValues(typeof(Card.Suit)))
+        
+        public Deck {
+            foreach (var suit in Enum.GetValues(typeof(Card.Suit)))
             {
-                foreach (Card.FaceValue faceValue in Enum.GetValues(typeof(Card.FaceValue)))
+                foreach (var faceValue in Enum.GetValues(typeof(Card.FaceValue)))
                 {
                     deckOfCards.Add(new Card(suit, faceValue));
                 }
             }
         }
+
         public Card draw()
         {
             //draw a Card, from Deck, remove it from Deck
@@ -26,15 +24,17 @@ namespace DeckOfCards
             deckOfCards.RemoveAt(0);
             return card;
         }
+        
         public void shuffle()   //does this need a parameter?
         {
             //loop to shuffle
-            List<Card> newDeck = new List<Card>();
-            Random random = new Random();
+            var newDeck = new List<Card>();
+            var random = new Random();
+            
             while (deckOfCards.Count > 0)
             {
                 int index = random.Next(deckOfCards.Count);
-                Card card = deckOfCards[index];
+                var card = deckOfCards[index];
                 deckOfCards.RemoveAt(index);
                 newDeck.Add(card);
             }
